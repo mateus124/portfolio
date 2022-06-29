b6a2d35c6bfc2b8dc3cb4a151be10594604ef845
Mateus Alves <107895139+mateus124@users.noreply.github.com>
2022-06-29 16:00:20 -0300

    animação em scrollreveal

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+/* Animação */
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+});
+
+sr.reveal(`.infor-eu`);
+sr.reveal(`.profile-border`, {delay: 500});
+sr.reveal(`.sobre-mim`, {delay: 600});
+sr.reveal(`.sociais`, {delay: 700});
+sr.reveal(`.experiencia`, {interval: 100, delay: 700});
+sr.reveal(`.extra`, {delay: 800});
